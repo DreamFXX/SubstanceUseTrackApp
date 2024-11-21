3b4bffbce73cd047c775f796ca87258723389eb7
DreamFXX <filipek.husak@gmail.com>
2024-11-21 21:11:09 +0100

    json appsettings + connString done

--- SubstanceUseTrackApp-Console/Program.cs
+++ SubstanceUseTrackApp-Console/Program.cs
@@ -2,8 +2,11 @@
 
 using Microsoft.Extensions.Configuration;
 
-IConfigurationRoot configuration = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json")
+IConfigurationRoot config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
     .AddEnvironmentVariables()
     .Build();
 
+string conn = config.GetValue<string>("DefaultConnection");
+
+Console.ReadKey();
